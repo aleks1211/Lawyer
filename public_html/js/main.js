e86520e1c570263770216ed44e11f0d094b5696a
Aleksandra Kostadinovic <aleksandra.kosta.sigma@gmail.com>
2023-12-17 19:00:19 +0100

    Team section done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,8 +1,8 @@
 
 
-$(document).ready(function(){
-    
-    //ANIMATION
+$(document).ready(function () {
+
+//ANIMATION
     function animation() {
         let  windowHeight = $(window).height();
         let scroll = $(window).scrollTop();
@@ -10,7 +10,6 @@ $(document).ready(function(){
             let position = $(this).offset().top;
             let animationName = $(this).attr('data-animation');
             let delay = $(this).attr('data-delay');
-
             if (position < windowHeight + scroll - 100) {
                 $(this).addClass(animationName);
                 $(this).css('animation-delay', delay);
@@ -23,10 +22,28 @@ $(document).ready(function(){
         animation();
     });
     animation();
+    //team-slider
 
-    
-    
-    
-    
+    if ($('.team-members-slider').length > 0) {
+        $(".team-members-slider").owlCarousel({
+            loop: true,
+            autoplay: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 0
+                },
+                992: {
+                    items: 2,
+                    margin: 30
+                }
+            }
+        }
+
+        );
+    }
+
+
+
 });
 
